b77f3a2f5df71b57de65ff03eb54c9e687674819
KimSangWook57 <asdewrp@naver.com>
2023-01-19 21:28:16 +0900

    이름 수정 및 파일 위치 변경

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -2,7 +2,7 @@ const showDice = () => {
     const n = Math.floor(Math.random() * 6 + 1);
     const s1 = document.querySelector("#s1");
 
-    s1.innerHTML = `<img src="./ex01/images/${n}.png" />`
+    s1.innerHTML = `<img src="./images/${n}.png" />`
     console.log(n);
 }
 
