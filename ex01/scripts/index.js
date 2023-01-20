53fe0a093526e2050704b5fec9d1d7ae9fa9196a
KimSangWook57 <asdewrp@naver.com>
2023-01-20 15:37:33 +0900

    주사위 던지기 마무리

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,10 +1,5 @@
-const showDice = () => {
-    const n = Math.floor(Math.random() * 6 + 1);
-    const s1 = document.querySelector("#s1");
-
-    s1.innerHTML = `<img src="./images/${n}.png" />`
-    console.log(n);
-}
+// 전역변수
+let n ;
 
 // 요소 보이기 함수
 const show = (dspS1, dspS2, dspS3, dspBt1, dspBt2) => {
@@ -21,8 +16,46 @@ const show = (dspS1, dspS2, dspS3, dspBt1, dspBt2) => {
     bt2.style.display = dspBt2;
 }
 
+// 주사위 번호 선택
+const showDice = () => {
+    n = Math.floor(Math.random() * 6 + 1);
+    const s1 = document.querySelector("#s1");
+    show("none", "block", "none", "none", "block")
+    s1.innerHTML = `<img src="./images/${n}.png" />`
+    console.log(n);
+}
+
+// 주사위 번호 결과 확인
+const showOk = () => {
+    const radios = document.querySelectorAll("input[type=radio]");
+
+    // 사용자가 선택한 숫자
+    let user_n;
+    for(let r of radios) {
+        if (r.checked) {
+            // 문자열을 숫자로 바꾸어 준다.
+            user_n = parseInt(r.value) ;
+            break; 
+        }
+    }
+    console.log(n, user_n);
+    // 결과 공개.
+    let tag;
+    // == -> 데이터 타입 상관 x, === -> 데이터 타입 상관 o.
+    if (n === user_n) {
+        tag = 'o';
+    } else {
+        tag = 'x';
+    }
+
+    document.querySelector("form").reset();
+
+    document.querySelector("#s1").innerHTML = `<img src="./images/${n}.png" />`
+    document.querySelector("#s3").innerHTML = `<img src="./images/${tag}.png" />`
+    show("block", "none", "block", "block", "none")
+}
 
 // DOM이 로드된 후...
 document.addEventListener("DOMContentLoaded", () => {
-    show("none", "block", "none", "block", "none")
+    show("none", "none", "none", "block", "none")
 });
